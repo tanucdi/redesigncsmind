fc28cd00f5085499506af3123581091f5ffffbc0
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-12-04 11:06:24 +0530

    Add files via upload

---
+++ Newhero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './newhero.css';
+
+const Newhero = () => {
+    return (
+        <div>
+            
+            <section id="heros">
+    <div class="heros-container">
+      <a href="index.html" class="hero-logo" data-aos="zoom-in"><img src="assets/img/log.png" alt=""/></a>
+      <h1 data-aos="zoom-in">Venturing Technology Innovations for
+Make in India.</h1>
+      <h2 data-aos="fade-up"><strong>Provide a network for all your needs with ease and fun using CSMIND and discover interesting features from us.</strong></h2>
+      <a href="#" class=" scrollto" data-aos="fade-up" class="btn btn-primary rounded-pill">Learn More</a>
+    </div>
+  </section>
+        </div>
+    )
+}
+
+export default Newhero
