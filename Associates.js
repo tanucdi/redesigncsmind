f3e30fe07cf2faa1d3bec25b3a85324af529e9e4
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-12-05 22:19:04 +0530

    Add files via upload

---
+++ Associates.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import './associates.css';
+
+const Associates = () => {
+    return (
+        <div>
+            
+            <section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Associates</h2>
+          <p>These are the gems associated with CSMIND.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="zoom-in">
+              <div class="member-img">
+                <img src="" class="img-fluid" alt=""/>
+              </div>
+              <div class="member-info">
+                <h4>HARSH SOLANKI</h4>
+                <span>MANAGING DIRECTOR & CO-FOUNDER</span>
+            <p></p>
+            </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="member-img">
+                <img src="" class="img-fluid" alt=""/>
+              </div>
+              <div class="member-info">
+                <h4>HARSH SOLANKI</h4>
+            <span>MANAGING DIRECTOR & CO-FOUNDER</span>
+            <p></p>
+          </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="zoom-in" data-aos-delay="200">
+              <div class="member-img">
+                <img src="" class="img-fluid" alt=""/>
+              </div>
+              <div class="member-info">
+                <h4>HARSH SOLANKI</h4>
+                <span>MANAGING DIRECTOR & CO-FOUNDER</span>
+                <p></p>
+             </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+        </div>
+    )
+}
+
+export default Associates
