8acbd9948f8d26688392f46fc307a53a857ef22e
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-12-05 12:24:01 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './contact.css';
+
+const Contact = () => {
+    return (
+        <>
+            
+<div class="page-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7 py-3 mb-5 mb-lg-0">
+        <div class="img-place w-lg-75 wow zoomIn">
+          <img src="media/illustration_contact.svg" alt=""/>
+        </div>
+      </div>
+      <div class="col-lg-5 py-3">
+        <h1 class="wow fadeInUp">Need a help? <br/>
+        Don't worry just contact us</h1>
+        
+        
+        
+            <div class="box">
+             
+              <h4 class="title">
+                  MAIL: <strong>help@csmind.in</strong>
+              </h4>
+                </div>
+            <div class="box">
+             
+              <h4 class="title">
+                  PHONE: <strong>8964896547</strong>
+              </h4>
+              </div>
+          
+          </div>
+    </div>
+  </div>
+</div>
+
+        </>
+    )
+}
+
+export default Contact
