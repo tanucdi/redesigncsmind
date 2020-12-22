2fba8f37f8eca66f6065a5d5a009913f5f1d0430
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-12-22 23:38:03 +0530

    PRIVACY POLICY PAGE

---
+++ Privacy.js
@@ -0,0 +1,159 @@
+import React from 'react'
+
+const Privacy = () => {
+    return (
+        <div>
+            
+        
+<div class="hero-caption">
+    <div class="container fg-white h-100">
+      <div class="row justify-content-center align-items-center text-center h-100">
+        <div class="col-lg-6">
+          <h2 class="mb-4 fw-medium">Privacy Policy <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-shield-fill-check" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 .5c-.662 0-1.77.249-2.813.525a61.11 61.11 0 0 0-2.772.815 1.454 1.454 0 0 0-1.003 1.184c-.573 4.197.756 7.307 2.368 9.365a11.192 11.192 0 0 0 2.417 2.3c.371.256.715.451 1.007.586.27.124.558.225.796.225s.527-.101.796-.225c.292-.135.636-.33 1.007-.586a11.191 11.191 0 0 0 2.418-2.3c1.611-2.058 2.94-5.168 2.367-9.365a1.454 1.454 0 0 0-1.003-1.184 61.09 61.09 0 0 0-2.772-.815C9.77.749 8.663.5 8 .5zm2.854 6.354a.5.5 0 0 0-.708-.708L7.5 8.793 6.354 7.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+</svg></h2>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4">This Privacy Policy describes how your personal information is
+collected, used, and shared when you visit or make a purchase from
+www. csmind. in (the “Site”). </p>
+  </div>
+   </div>
+  </div>
+
+<br/>
+
+
+  <div class="container">
+  <h3 class="mb-4 haha"><strong>PERSONAL INFORMATION WE COLLECT :</strong></h3>
+    <div class="row">
+    
+    <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4"> When someone wishes to take a service from the CSMIND , we ask for
+certain information from you, including your name, address, payment
+information email address, and phone number.  We refer to this
+information as “Order Information.”
+<br/>
+When we talk about “Personal Information” in this Privacy Policy, we
+are talking both, about Device Information and Order Information. </p>
+  </div>
+    </div>
+</div>
+
+
+<div class="container">
+  <h3 class="mb-4 haha"><strong>HOW DO WE USE YOUR PERSONAL INFORMATION ?</strong></h3>
+    <div class="row">
+    
+    <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4"> When someone wishes to take a service from the CSMIND , we ask for
+certain information from you, including your name, address, payment
+information email address, and phone number.  We refer to this
+information as “Order Information.”
+<br/>
+When we talk about “Personal Information” in this Privacy Policy, we
+are talking both, about Device Information and Order Information. </p>
+  </div>
+    </div>
+</div>
+
+
+<div class="container">
+  <h3 class="mb-4 haha"><strong>SHARING YOUR PERSONAL INFORMATION :</strong></h3>
+    <div class="row">
+    <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4"> We do notshare any of your your Personal Information with third
+parties.We may be liable to share your Personal Information to comply
+with applicable laws and regulations, lawful request for information we
+receive, or to otherwise protect our rights.
+<br/>
+we may use your Personal Information to provide you with , we believe
+may be of interest to you. 
+You can opt out of targeted advertising by:
+As such ,you can opt out of some of these services by visiting the site or
+witting us a mail. <br/>
+We may seek your additional information from other entities who might
+be maintaining your information , with whom we might collaborate ,
+with your due permission ,so as to improve the services we offer to you .
+We do not share such information further to any one for any purpose.
+We do not access any information , of any kind , with out your
+knowledge and consent . </p>
+  </div>
+    </div>
+</div>
+
+<div class="container">
+  <h3 class="mb-4 haha"><strong>DO NOT TRACK :</strong></h3>
+    <div class="row">
+    
+    <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4"> Please note that we do not alter our Site’s data collection and use
+practices when we see a Do Not Track signal from your browser. </p>
+  </div>
+    </div>
+</div>
+
+
+<div class="container">
+  <h3 class="mb-4 haha"><strong>YOUR RIGHTS :</strong></h3>
+    <div class="row">
+    
+    <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4"> You have the right to access personal information we hold about , given
+by you , and to ask that your personal information be corrected,
+updated, or deleted. If you would like to exercise this right, please
+contact us through the <strong>contact : info@csmind.in</strong> </p>
+  </div>
+    </div>
+</div>
+
+
+<div class="container">
+  <h3 class="mb-4 haha"><strong>DATA RETENTION :</strong></h3>
+    <div class="row">
+    <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4"> When you place an order through the Site, we will maintain your Order
+Information for our records unless and until you ask us to delete this
+information.<br/>
+The Site is not intended for individuals under the age of 10 years. </p>
+  </div>
+    </div>
+</div>
+
+
+<div class="container">
+  <h3 class="mb-4 haha"><strong>CHANGES :</strong></h3>
+    <div class="row">
+    <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4">We may update this privacy policy from time to time in order to reflect,
+for example, changes to our practices or for other operational, legal or
+regulatory reasons. </p>
+  </div>
+    </div>
+</div>
+
+
+<div class="container">
+  <h3 class="mb-4 haha"><strong>CONTACT US :</strong></h3>
+    <div class="row">
+    <div class="col-lg-12 offset-lg-1 py-3 wow fadeInUp">
+        <p class="mb-4">For more information about our privacy practices, if you have questions,
+or if you would like to make a complaint, please<b> contact </b>us by <b> e-mail </b> at
+<strong> profpkchande@gmail.com</strong> or <strong> info@csmind.in </strong> . </p>
+  </div>
+    </div>
+</div>
+
+        </div>
+    )
+}
+
+export default Privacy
