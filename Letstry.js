dcee6983c20fc132f7320fea2cba0d87f94e0b36
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-12-25 22:35:44 +0530

    new hero

---
+++ Letstry.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './letstry.css';
+
+const Letstry = () => {
+    return (
+        <div>
+<div class="page-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7 py-3">
+        <div class="img-place">
+          <img src="SVG/okay.png" alt="img"/>
+        </div>
+      </div>
+      <div class="col-lg-5 py-3 mt-lg-5">
+        <div class="iconic-list">
+          <div class="iconic-item">
+            <div class="iconic-content">
+              <h5>Venturing Technology Innovations</h5>
+              <p>PROVIDE A NETWORK FOR ALL YOUR NEEDS WITH EASE AND FUN USING CSMIND AND DISCOVER INTERESTING FEATURES FROM US.</p>
+              <a class="read-more">Read More <i class="icofont-long-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section id="call-to-action" class="wow fadeInUp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center text-lg-left">
+            <h3 class="cta-title">Mission and Vision <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
+  <path d="M9.669.864L8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193l.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
+  <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+</svg></h3>
+            <p class="cta-text">Pursue technology innovations in an eco-system fueled by talent from industry and academia to create systems of the future for the socio- economic growth to strengthen make in India. </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+
+        </div>
+    )
+}
+
+export default Letstry
