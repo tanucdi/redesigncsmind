c7073fd1f4ede02aa2bacf6bda7c486c8b486410
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-12-29 00:48:59 +0530

    Add files via upload

---
+++ Affi.js
@@ -0,0 +1,253 @@
+import React from 'react'
+import './affi.css';
+
+const Affi = () => {
+    return (
+        <div>
+            <section id="featured" class="featured">
+      <div class="container">
+      <div class="section-title" data-aos="fade-up">
+          <h2>Affiliates and Experts</h2>
+          <p>These are our gems and we are happy to have them.</p>
+        </div>
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+            <div class="tab-content">
+              <div class="tab-pane active show" id="tab-1">
+                <figure>
+                  <img src="SVG/kumar-vijay-2013.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+              <div class="tab-pane" id="tab-2">
+                <figure>
+                  <img src="SVG/Manoj.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+              <div class="tab-pane" id="tab-3">
+                <figure>
+                  <img src="SVG/Kavindra.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+              <div class="tab-pane" id="tab-4">
+                <figure>
+                  <img src="SVG/Rajesh.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+              <div class="tab-pane" id="tab-5">
+                <figure>
+                  <img src="SVG/Parag.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 mt-4 mt-lg-0" data-aos="fade-left">
+            <ul class="nav nav-tabs flex-column">
+              <li class="nav-item">
+                <a class="nav-link active show" data-toggle="tab" href="#tab-1">
+                  <h4>Prof. Vijay Kumar</h4>
+                  <p>Curator’s Distinguished Research Professor(Retired), 
+Director: International Degree Collaboration Program, 
+ACM Distinguished Scientist
+SCE, Computer Science Electrical Engineering
+University of Missouri.
+</p><hr/>
+<a data-toggle="collapse" href="#faq8" class="collapsed">Words From Prof. Vijay Kumar  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq8" class="collapse" data-parent=".faq-list">
+              <p>My association with Dr. P K Chande goes back more than 25 years ago. We first met in a conference he was organizing. At that time he was the director of the well-known institute called SGITS, Indore. He invited me to give a talk on Database systems. It was a successful event and since then we kept in touch through email, Skype and through other communication channels. I was impressed with his innovative ideas on information management discipline which led to tighter collaboration. This resulted in offering joint workshops and seminars on mobility, databases, etc. I visited him at several occasions and we travelled together to universities and companies who invited us to organize academic events. Dr. Chande did not remain confined to our collaboration, he diversified his activities in administration, software development, running a number of academic institutes and universities very successfully without slowing down in research and development work. He is a first class innovator. Recently he is focusing in AI and to this end, he is building a highly useful web-based system called CSMIND. It is a wonderful system which provides opportunities to young scientists, upcoming industries, universities, teachers, educationists, etc., to materialize their inventions, products innovations, etc., and work with them to create systems for the future. The world, especially, Hindustan needs such system to lead many countries in the field of education, research and development. I encourage our young generations and established researchers to connect with CSMIND to make our country a leader in technology. I am happy to be in the vicinity of Dr. Chande’s sphere of control.
+  </p>
+            </div>
+                </a>
+              </li>
+              <li class="nav-item mt-2">
+                <a class="nav-link" data-toggle="tab" href="#tab-2">
+                  <h4>Mr. Manoj Shrivastav </h4>
+                  <p>Former Executive Director, MTS India, Former Group VP, Reliance ADA Group.</p>
+                  <hr/>      <a data-toggle="collapse" href="#faq7" class="collapsed">Words From Mr. Manoj Shrivastav  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq7" class="collapse" data-parent=".faq-list">
+              <p>Professor Chande has been my first mentor right from the days I was an engineering student. When I first came in touch with him I wanted to write an article on superconductivity for a national contest. Someone suggested me to get in touch with him. I wondered, “How can a Professor of Computer Engineering and Electronics” guide me on superconductivity. 
+I was so wrong. He not only guided me helped me re-write the draft that I went with and the final essay was way valuable in fact, so valuable that it got me a national prize. Since then, I worked for several years on several research projects and when I moved on to the corporate world he motivated me to make the change.
+He has been a role model for many youths for research, teaching and professional growth. I feel that such mentors are available to all youth and CSMIND will bridge this gap for the youths.  </p>
+            </div>
+                </a>
+              </li>
+              <li class="nav-item mt-2">
+                <a class="nav-link" data-toggle="tab" href="#tab-3">
+                  <h4>Mr. Kavindra Sharma</h4>
+                  <p>Vice-President and Head-Consulting, L&T infotech (Retd.) Sr. Adj. Professor, NMIMS Indore.</p>
+                  <hr/>      <a data-toggle="collapse" href="#faq9" class="collapsed">Words From Mr. Kavindra Sharma  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq9" class="collapse" data-parent=".faq-list">
+              <p>
+              CS MIND is a unique technology innovation platform lead by Prof Chande , where his  team is shapinng breakthrough ideas for the benefit of the society. CSMIND continues to harness the talent from academia and industry to realize Make in India mission.  </p>
+            </div>
+                </a>
+              </li>
+              <li class="nav-item mt-2">
+                <a class="nav-link" data-toggle="tab" href="#tab-4">
+                  <h4>Mr. Rajesh Bhargav </h4>
+                  <p>Chairman CHL hospitals ,Indore </p>
+                  <hr/>      <a data-toggle="collapse" href="#faq3" class="collapsed">Words From Mr. Rajesh Bhargav  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq3" class="collapse" data-parent=".faq-list">
+              <p>
+              We are multispeciality hospitals and make use of state-of -the-art equipment in our healthcare practices. The CSMIND associates with us in their healthcare technology innovations for increased efficacy,  suiting  Indian needs and thus  making a  global impact.</p>
+            </div>
+                </a>
+              </li>
+              <li class="nav-item mt-2">
+                <a class="nav-link" data-toggle="tab" href="#tab-5">
+                  <h4>Dr. Parag Kulkarni</h4>
+                  <p>Founding CEO and Chief Scientist of iknowlation Research Labs - a vibrant Machine Learning Product.</p>
+                  <hr/>        <a data-toggle="collapse" class="collapsed" href="#faq1">Words From Dr. Parag Kulkarni  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq1" class="collapse" data-parent=".faq-list">
+              <p>
+              Dr Chande is not only academician and researcher but also motivator, entrepreneur and engineer by heart. He has been transforming the lives of millions of students and empowered them to become real successful engineers  in this world full of noise. In true sense today we need gurus and guides like him. His initiative CS MIND is about making his golden advice available for everyone and democratization of technical creativity. We have co-authored books and I am also blessed with his golden words… His CS MIND as a venture is a  new endeavour for  universities, youth and industry to witness advances in technology innovations with make in India spirit  and shall benefit all of us.  </p>
+            </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+<br/>
+        <div class="row">
+          <div class="col-lg-6 mt-4 mt-lg-0" data-aos="fade-left">
+            <ul class="nav nav-tabs flex-column">
+              <li class="nav-item">
+                <a class="nav-link active show" data-toggle="tab" href="#tab-6">
+                  <h4>Dr. Rajesh Sodani</h4>
+                  <p>Principal at Shri Vaishnav Polytechnic College indore.</p>
+                  <hr/>        <a data-toggle="collapse" href="#faq6" class="collapsed">Words From Dr. Rajesh Sodani  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq6" class="collapse" data-parent=".faq-list">
+              <p>
+              As a practicing engineer in general safety, I did my PhD research with prof. Chande, and had come up with a new D.M. Model.
+I am pleased to know that this research has come to a reality by the efforts of the CS MIND team. 
+This has a great potential to make indian cities resilient and become a role model in the world.
+ </p>
+            </div>
+                </a>
+              </li>
+              <li class="nav-item mt-2">
+                <a class="nav-link" data-toggle="tab" href="#tab-7">
+                  <h4>Mr. Vivek Deshpande</h4>
+                  <p>Enterprise Resources Planning Specialist at University of Wisconsin-Extension at UW-Shared Services.</p>
+                  <hr/>     <a data-toggle="collapse" href="#faq5" class="collapsed">Words From Mr. Vivek Deshpande  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq5" class="collapse" data-parent=".faq-list">
+              <p>
+              CSMIND is a platform which says-You are known by what you create. Yes in today time disruptive technological creation is the key for the future. And it is a team work with accomplished people. I am sure any body associating with CS Mind will be highly benefited in terms of mentoring, innovations, research and willing to come up with a venture.  </p>
+            </div>
+                </a>
+              </li>
+              <li class="nav-item mt-2">
+                <a class="nav-link" data-toggle="tab" href="#tab-8">
+                  <h4>Dr. Sat Parashar</h4>
+                  <p>Former Director, IIM Indore.</p>
+                  <hr/>       <a data-toggle="collapse" href="#faq10" class="collapsed">Words From Dr. Sat Parashar  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq10" class="collapse" data-parent=".faq-list">
+              <p>
+              Under the leadership of Prime Minister Narendra Modi, India is reinventing itself. ‘Make in India’ is part
+of the same vision. No national vision can be realized unless adopted and executed at the local levels. I
+am delighted to learn that Professor Pradip Chande has decided to dedicate himself to help realize this
+vision by pursuing technology innovations in an eco-system fueled by talent from industry and
+academia. I have known Professor Chande, personally and professionally, for over a decade and half.
+Technology innovation has been his second nature. I wish him every success.
+  </p>
+            </div>
+                </a>
+              </li>
+              <li class="nav-item mt-2">
+                <a class="nav-link" data-toggle="tab" href="#tab-9">
+                  <h4>Mr. Sanjeev Agarwal</h4>
+                  <p>Vice-President Impetus, Indore</p>
+                  <hr/>      <a data-toggle="collapse" href="#faq2" class="collapsed">Words From Mr. Sanjeev Agarwal  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq2" class="collapse" data-parent=".faq-list">
+              <p>
+              Today I represent a large multinational IT Company from India. I have witnessed the innovative solutions developed by CS MIND to enable large companies with large man power to deploy appropriate teams on upcoming projects globally. This AI based approach will help to increase their professional efficiency to compete.  </p>
+            </div>
+                </a>
+              </li>
+              <li class="nav-item mt-2">
+                <a class="nav-link" data-toggle="tab" href="#tab-10">
+                  <h4>Prof Apurv Gaiwak </h4>
+                  <p>Coordinator, CIDI ,SGSITS Indore</p>
+                  <hr/>       <a data-toggle="collapse" href="#faq4" class="collapsed">Words From Prof Apurv Gaiwak  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 18 18">
+  <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+</svg></a>
+            <div id="faq4" class="collapse" data-parent=".faq-list">
+              <p>
+              C.S. Mind is a venture to bring new ideas to reality using innovative technology. I am the Coordinator of CIDI, SGSITS, and CS Mind is associated with us for realizing their creativity in various fields to the benefit of society. 
+CS Mind motivates & leads a large group of students & helps make them their carrier.  </p>
+            </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6" data-aos="fade-right">
+            <div class="tab-content">
+              <div class="tab-pane active show" id="tab-6">
+                <figure>
+                  <img src="SVG/DrRajesh.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+              <div class="tab-pane" id="tab-7">
+                <figure>
+                  <img src="SVG/Vivek.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+              <div class="tab-pane" id="tab-8">
+                <figure>
+                  <img src="SVG/Sat.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+              <div class="tab-pane" id="tab-9">
+                <figure>
+                  <img src="SVG/Sanjeev.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+              <div class="tab-pane" id="tab-10">
+                <figure>
+                  <img src="SVG/Apurv.jpg" alt="" class="img-fluid"/>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br/>
+    </section>
+<hr/><hr/>
+<br/>
+        <ul class="faq-list">
+        </ul>
+
+
+        </div>
+    )
+}
+
+export default Affi
