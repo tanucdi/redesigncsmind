d009c50f806836a5b153abee77b9d50b9c210edb
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-12-31 15:07:39 +0530

    Add files via upload

---
+++ Faq.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Faq = () => {
+    return (
+        <div>
+            
+            <section id="faq" class="faq">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Frequently Asked Questions</h2>
+        </div>
+
+        <ul class="faq-list">
+
+          <li data-aos="fade-up">
+            <a data-toggle="collapse" class="collapsed" href="#faq1">What is CS MIND ? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+            <div id="faq1" class="collapse" data-parent=".faq-list">
+              <p>
+              A technology   innovation platform for academia,industry , and ,youth. </p>
+            </div>
+          </li>
+
+          <li data-aos="fade-up" data-aos-delay="100">
+            <a data-toggle="collapse" href="#faq2" class="collapsed">What is venturing ? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+            <div id="faq2" class="collapse" data-parent=".faq-list">
+              <p>
+              It helps the youth   to dream big and realize their innovative ideas  </p>
+            </div>
+          </li>
+
+          <li data-aos="fade-up" data-aos-delay="300">
+            <a data-toggle="collapse" href="#faq4" class="collapsed">Can I get personal career mentoring ? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+            <div id="faq4" class="collapse" data-parent=".faq-list">
+              <p>
+              Individual mentoring  sessions for carrier guidance , studies , research , projects are fecilitated    </p>
+            </div>
+          </li>
+
+          <li data-aos="fade-up" data-aos-delay="400">
+            <a data-toggle="collapse" href="#faq5" class="collapsed">. What is the training program available ?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+            <div id="faq5" class="collapse" data-parent=".faq-list">
+              <p>
+              A program to train individuals for exploring path of innovation ,product development, and realizing venture is provided. </p>
+            </div>
+          </li>
+
+
+        </ul>
+
+      </div>
+    </section>
+
+        </div>
+    )
+}
+
+export default Faq
