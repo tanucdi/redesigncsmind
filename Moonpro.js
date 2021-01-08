ad25a04799ea4920f25da770b71d14eb7895358c
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2021-01-08 22:50:10 +0530

    our page with my pic and yours...

---
+++ Moonpro.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import './moonpro.css';
+import './associates.css';
+
+const Moonpro = () => {
+    return (
+        <div>
+            
+            <main class="bg-light" >
+
+<div class="page-hero-section bg-image hero-mini" >
+  <div class="hero-caption">
+    <div class="container fg-white h-100">
+      <div class="row justify-content-center align-items-center text-center h-100">
+        <div><h1>MOONSITE SOLUTIONS</h1> 
+          <h2>make your product happen</h2> <hr/>
+          <h4>We creates website for your business model by including all its core values.</h4>
+       <a href="https://www.moonsitesolutions.tech">   <h4 class="cta-title">www.moonsitesolutions.tech</h4> </a>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="page-sectionss">
+  <div class="container">
+    <div class="row">
+<section id="call-to-actionss" class="wow fadeInUp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center text-lg-left">
+            <h3 class="cta-title">Incubate Your Needs </h3>
+            <h5 class="cta-texts">IT AT CS MIND - GO FOR IT WITH 21ST CENTURY </h5>
+            <p class="cta-text">Its all about making your processes digital which is a new norm now. Rethink redesign reengineer to transform your business applications using AI and IT to remain competitive in this changing world. We for to engage with us for your technology development needs. Our talented team shall be eager to help you out. Send Your queries & requirements at info@csmind.in or visit <a href="https://www.moonsitesolutions.tech">www.moonsitesolutions.tech</a>.</p>
+            <a href="#" class=" scrollto" data-aos="fade-up" class="btn btn-outline-primary rounded-pill">Enquire Now</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    </div>
+    </div>
+    </div>
+
+</main>
+
+<section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Folks Behind Cs-Mind</h2>
+          <p>Creators Areana</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-5 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="zoom-in">
+              <div class="member-img">
+                <img src="SVG/profilepic.jpg" class="img-fluid" alt=""/>
+              </div>
+              <div class="member-info">
+                <h4>Tanishk Chaturvedi</h4>
+                <span>MANAGING DIRECTOR & CO-FOUNDER</span>
+            <p></p>
+            </div>
+            </div>
+          </div>
+        <div class="col-lg-2">
+          </div>
+
+          <div class="col-lg-5 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="zoom-in" data-aos-delay="200">
+              <div class="member-img">
+                <img src="SVG/profilepic.jpg" class="img-fluid" alt=""/>
+              </div>
+              <div class="member-info">
+                <h4>HARSH SOLANKI</h4>
+                <span>MANAGING DIRECTOR & CO-FOUNDER</span>
+                <p></p>
+             </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    
+        </div>
+    )
+}
+
+export default Moonpro
